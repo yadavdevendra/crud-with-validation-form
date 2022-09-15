978301e6a248b4e360aaf683526ff7e9904b494a
yadavdevendra <devendrayadav2022aug@cedcommerce.com>
2022-09-15 11:20:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,68 +3,79 @@ import { Form } from "./components/Form";
 // import { data } from "./components/Data";
 const App = () => {
   const [data, setData] = useState([]);
-  const [tableData, setTableData] =useState([]) 
-  const [id, setId] = useState([]); 
-  const [phone, setphone] = useState([]); 
-  const [email, setEmail] = useState([]); 
-  const [companyname, setCompanyname] = useState([]); 
-  const [newData, setNewData] = useState({
-    id: "",
-    username: "",
-    phoneNumber: "",
-    email: "",
-    companyName: "",
-  });
-
-   useEffect(() => {
-     fetch(`https://jsonplaceholder.typicode.com/users`)
-       .then((resp) => resp.json())
-       .then((data) => {
+  const [tableData, setTableData] = useState([]);
+  const [id, setId] = useState([]);
+  const [name, setName] = useState("");
+  const [username, setUserName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [companyname, setCompanyname] = useState("");
+  // const [newData, setNewData] = useState({
+  //   id: "",
+  //   username: "",
+  //   phoneNumber: "",
+  //   email: "",
+  //   companyName: "",
+  // });
+console.log(name,username);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((resp) => resp.json())
+      .then((data) => {
         console.log(data);
-         setData(data); 
-       });
-   },[]);
+        setData(data);
+      });
+  }, []);
 
-   const handlename = (e) => {
-     const { id, value } = e.target;
-     setNewData({ ...newData, [id]: value });
-     console.log(value);
-   };
-    const handleusername = (e) => {
-      const { id, value } = e.target;
-      setNewData({ ...newData, [id]: value });
-      console.log(value);
+  const handlename = (e) => {
+    const { id, value } = e.target;
+    setName(value);
+    console.log(value);
+  };
+  const handleusername = (e) => {
+    const { id, value } = e.target;
+    setUserName(value);
+    console.log(value);
+  };
+  const handlephone = (e) => {
+    const { id, value } = e.target;
+    setPhone(value);
+    console.log(value);
+  };
+  const handleemail = (e) => {
+    const { id, value } = e.target;
+    setEmail(value);
+    console.log(value);
+  };
+  const handlecompanyname = (e) => {
+    const { id, value } = e.target;
+    setCompanyname(value);
+    console.log(value);
+  };
+
+  const Add = (e) => {
+    e.preventDefault();
+    // setData([...data, { ...newData, id: Math.random() }]); // for giving the new data some random id
+const max = 50
+// if(max>10){
+//   return max
+// }
+    const newData = {
+      // for enptying the form
+      id: Math.floor(Math.random() * max),
+      name,
+      username,
+      phone,
+      email,
+      company: { name: companyname },
     };
-     const handlephone = (e) => {
-       const { id, value } = e.target;
-       setNewData({ ...newData, [id]: value });
-       console.log(value);
-     };
-       const handleemail = (e) => {
-         const { id, value } = e.target;
-         setNewData({ ...newData, [id]: value });
-         console.log(value);
-       };
-        const handlecompanyname = (e) => {
-          const { id, value } = e.target;
-          setNewData({ ...newData, [id]: value });
-          console.log(value);
-        };
-
-   const Add = (e) => {
-     e.preventDefault();
-     setData([...data, { ...newData, id: Math.random() }]); // for giving the new data some random id
-     setNewData({
-       // for enptying the form
-       
-       userName: "",
-       phoneNumber: "",
-       email: "",
-       companyName: "",
-     });
-     setData(...[data],newData)
-     console.log(newData);
-   };
+    if (true){
+      return false
+    }else{
+    setData([...data, newData ]);
+    }
+    console.log("data ",data.id,newData.id);
+  };
   return (
     <>
       <div className="container">
@@ -73,21 +84,10 @@ const App = () => {
             <h1>Form Handling With Validations</h1>
             <label>
               Name:
-              <input
-                type="text"
-                id="name"
-                onChange={handlename}
-                value={newData.fullName}
-              />
+              <input type="text" id="name" onChange={handlename} value={name} />
             </label>
             <label>
-              UserName:{" "}
-              <input
-                type="text"
-                id="username"
-                onChange={handleusername}
-                value={newData.fullName}
-              />
+              <input type="text" id="username"onChange={handleusername}value={username}/>
             </label>
             <label>
               Email:{" "}
@@ -95,7 +95,7 @@ const App = () => {
                 type="text"
                 id="email"
                 onChange={handleemail}
-                value={newData.fullName}
+                value={email}
               />
             </label>
             <label>
@@ -104,7 +104,7 @@ const App = () => {
                 type="text"
                 id="phone"
                 onChange={handlephone}
-                value={newData.fullName}
+                value={phone}
               />
             </label>
             <label>
@@ -113,11 +113,11 @@ const App = () => {
                 type="text"
                 id="companyname"
                 onChange={handlecompanyname}
-                value={newData.fullName}
+                value={companyname}
               />
             </label>
             <button onClick={Add}>Add User Deatail</button>
-            <table className="">
+            <table className="table">
               <thead>
                 <tr>
                   <th id="tr">Id </th>
